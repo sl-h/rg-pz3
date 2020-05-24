91abb65d61ca73efd1f019ae96d8124ac4a06c33
aleksandar-r <aleksandar.h@outlook.com>
2020-05-24 22:27:19 +0200

    DrawMapSurfice

---
+++ IveGrid3D/IveGrid3D/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
+
+namespace IveGrid3D
+{
+    public partial class MainWindow
+    {
+        private const string ImagePath = @"D:\fax\rg_pz3\IveGrid3D\IveGrid3D\Assets\Images\map.jpg";
+        private readonly Model3DGroup mainModel3DGroup = new Model3DGroup();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddModelGroupToViewPort();
+            Viewport.PanGesture = new MouseGesture(MouseAction.LeftClick);
+            Viewport.RotateGesture = new MouseGesture(MouseAction.RightClick);
+            DrawMapSurface();
+
+        }
+
+        private void AddModelGroupToViewPort()
+        {
+            var modelVisual = new ModelVisual3D { Content = mainModel3DGroup };
+            Viewport.Children.Add(modelVisual);
+        }
+
+        private void DrawMapSurface()
+        {
+            var builder = new MeshBuilder();
+            builder.AddCube(BoxFaces.Top);
+            var cube = builder.ToMesh();
+
+            var mat = new DiffuseMaterial { Brush = new ImageBrush(new BitmapImage(new Uri(ImagePath))) };
+
+            var surfaceModel = new GeometryModel3D(cube, mat)
+            {
+                BackMaterial = mat,
+                Transform = new ScaleTransform3D(-10, 10, 10)
+            };
+
+            mainModel3DGroup.Children.Add(surfaceModel);
+        }
+    }
+}
---
+++ IveGrid3D/IveGrid3D/UIControls/MainWindow.MainWindow.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+
+namespace IveGrid3D
+{
+    public partial class MainWindow
+    {
+        private readonly int zoomMax = 7;
+
+        private Point start;
+        private Point diffOffset;
+        private int zoomCurrent = 1;
+
+
+        private void OnLeftMouseButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Viewport.ReleaseMouseCapture();
+        }
+
+        private void OnLeftMouseButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Viewport.CaptureMouse();
+            start = e.GetPosition(this);
+            //diffOffset.X = TranslateContent.OffsetX;
+            //diffOffset.Y = TranslateContent.OffsetY;
+        }
+
+        private void Pan(object sender, MouseEventArgs e)
+        {
+            if (Viewport.IsMouseCaptured)
+            {
+                var end = e.GetPosition(this);
+                var offsetX = end.X - start.X;
+                var offsetY = end.Y - start.Y;
+                var w = this.Width;
+                var h = this.Height;
+                var translateX = (offsetX * 100) / w;
+                var translateY = -(offsetY * 100) / h;
+                var position = new Matrix3D();
+                //position.OffsetX = diffOffset.X + (translateX / (100 * ScaleContent.ScaleX));
+                //position.OffsetY = diffOffset.Y + (translateY / (100 * ScaleContent.ScaleX));
+                //Viewport.Camera.Transform.SetCurrentValue();
+                // TranslateContent.OffsetX = diffOffset.X + (translateX / (100 * ScaleContent.ScaleX));
+                //TranslateContent.OffsetY = diffOffset.Y + (translateY / (100 * ScaleContent.ScaleX));
+            }
+        }
+        private void MouseWheal(object sender, MouseWheelEventArgs e)
+        {
+            double scaleX;
+            double scaleY;
+            //if (e.Delta > 0 && zoomCurrent < zoomMax)
+            //{
+            //    scaleX = ScaleContent.ScaleX + 0.1;
+            //    scaleY = ScaleContent.ScaleY + 0.1;
+            //    zoomCurrent++;
+            //    ScaleContent.ScaleX = scaleX;
+            //    ScaleContent.ScaleY = scaleY;
+            //}
+            //else if (e.Delta <= 0 && zoomCurrent > -zoomMax)
+            //{
+            //    scaleX = ScaleContent.ScaleX - 0.1;
+            //    scaleY = ScaleContent.ScaleY - 0.1;
+            //    zoomCurrent--;
+            //    ScaleContent.ScaleX = scaleX;
+            //    ScaleContent.ScaleY = scaleY;
+            //}
+        }
+    }
+}
